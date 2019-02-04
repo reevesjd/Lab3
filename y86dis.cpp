4d010942811a1e65bbf861c080d9e36880863d25
Justin Reeves <reevesjd@appstate.edu>
2019-02-04 16:57:46 -0500

    correctly parse address and instruction

--- y86dis.cpp
+++ y86dis.cpp
@@ -2,18 +2,39 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 void diss(std::ifstream &r)
 {
     std::string line;
     while(r)
     {
+        //int linePos = 0;
+        std::string delim = ": ";
+        std::string tok;
+        //linePos = line.find(delim);
         //std::string s = getline(r);
         //std::stringstream stm(line);
-        std::getline(r, line, '\n');
-        std::cout << line + "\n";
+        std::getline(r, line);//, ": ");
+        std::istringstream stm(line);
+        int lineNum = 1;
+        while(std::getline(stm, tok, ':'))
+        {
+            if (lineNum % 2 == 0)
+            {
+                tok = tok.substr(1, tok.length());
+            }
+            std::cout << tok << "\n";
+            lineNum++;
+        }
+        //std::string tok1 = line.substr(0, linePos);    
+        //std::string tok2 = line.substr(line.find(delim) + 2);
+        //line.trim();
+        //std::cout << tok1 + "\n";
+        //std::cout << tok2 + "\n";
     }
 }
 
