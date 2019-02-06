5d6824db74782c84763ded36e9d4305a17df4720
Justin Reeves <reevesjd@appstate.edu>
2019-02-06 00:48:39 -0500

    Remove unnecessary code and add comments

--- y86dis.cpp
+++ y86dis.cpp
@@ -1,3 +1,11 @@
+/*
+ * Y86 Disassembler
+ * 
+ * Author: Justin Reeves
+ * Date: 2/6/2019
+ */
+
+
 #include <cstdlib>
 #include <cstdio>
 #include <fstream>
@@ -8,9 +16,10 @@
 #include <algorithm>
 #include <bits/stdc++.h>
 
+//Flips the endianness of passed addresses or destinations
 void flipEndOther(char *tok)
 {
-   for (int i = 0, j = 15; i < 8; i = i + 2, j = j - 2)
+   for (int i = 0, j = 15; i < 8; i = i + 2, j = j - 2)     //i tracks the lower bytes, j tracks the upper
     {
         char temp = tok[i];
         tok[i] = tok[j - 1];
@@ -20,7 +29,7 @@ void flipEndOther(char *tok)
         tok[j] = temp;
     }
 
-    int k = 0;
+    int k = 0;                                              //Used to count number of leading 0s
     while (tok[k] == '0')
     {
         k++;
@@ -28,7 +37,7 @@ void flipEndOther(char *tok)
 
     if (k == 16)
     {
-        std::cout << "0x0";
+        std::cout << "0x0";                                 //Prints 0x0 if all numbers in value are 0s
     }
     
     else
@@ -41,26 +50,10 @@ void flipEndOther(char *tok)
     }
 }
 
+//Flips the endianness of a passed quad value
 void flipEndQuad(char *tok)
 {
-    /*char arr [16];
-    arr[16] = '\0';
-    for (int i = 0; i < 16; i++)
-    {
-        arr[i] = tok[i];
-    }*/
-
-    /*for (int i = 0, j = 15; i < 8; i = i + 2, j = j - 2)
-    {
-        char temp = arr[i];
-        arr[i] = arr[j - 1];
-        arr[j - 1] = temp;
-        temp = arr[i + 1];
-        arr[i + 1] = arr[j];
-        arr[j] = temp;
-    }*/
-
-    for (int i = 0, j = 15; i < 8; i = i + 2, j = j - 2)
+    for (int i = 0, j = 15; i < 8; i = i + 2, j = j - 2)    //i tracks the lower bytes, j tracks the upper
     {
         char temp = tok[i];
         tok[i] = tok[j - 1];
@@ -71,7 +64,7 @@ void flipEndQuad(char *tok)
     }
 }
 
-//Add spaces or commas?
+//Compares a passed character to ASCII values of register identifiers, printing the name of the register
 void getRegister(char a)
 {
     std::string regOut;
@@ -150,12 +143,13 @@ void getRegister(char a)
     std::cout << regOut;
 }
 
+//Checks the passed values to determine which to fetch a register ID for and in what order
 void checkRegisters(char a, char b)
 {
     if (a != 0 && b != 0)
     {
         getRegister(b);
-        std::cout << ", ";
+        std::cout << ", ";                      //Places a comma between registers when both need to be fetched
         getRegister(a);
     }
 
@@ -166,8 +160,6 @@ void checkRegisters(char a, char b)
 
     else if (a == 0 && b != 0)
     {
-        //Doesn't need to output F, just placeholder
-        //std::cout << "F";
         getRegister(b);
     }
 
@@ -177,22 +169,23 @@ void checkRegisters(char a, char b)
     }
 }
 
-void oneByte(/*const*/ char* tok)
+//Determines if an instruction that is 1 byte long is a halt, nop, or return
+void oneByte(char* tok)
 {
     std::string instOut;
     if (tok[0] == '0' && tok[1] == '0')
     {
-        instOut = "halt\n";
+        instOut = "halt\n";                     //Prints the data for a HALT code
     }
 
     else if (tok[0] == '1' && tok[1] == '0')
     {
-        instOut = "nop\n";
+        instOut = "nop\n";                      //Prints the data for a no operation
     }
 
     else if (tok[0] == '9' && tok[1] == '0')
     {
-        instOut = "ret\n";
+        instOut = "ret\n";                      //Prints the data for a return
     }
 
     else
@@ -203,32 +196,33 @@ void oneByte(/*const*/ char* tok)
     std::cout << instOut;
 }
 
-void twoBytes(/*const*/ char* tok)
+//Determines if an instruction that is 2 bytes long is a rrmovq, conditional move, operation, push, or pop
+void twoBytes(char* tok)
 {
     std::string instOut;
     if (tok[0] == '2')
     {
         switch (tok[1])
         {
-            case '0':   instOut = "rrmovq";
+            case '0':   instOut = "rrmovq";         //Prints the data for a register move
                         break;
 
-            case '1':   instOut = "cmovle";
+            case '1':   instOut = "cmovle";         //Prints the data for a move less than or equal
                         break;
 
-            case '2':   instOut = "cmovl";
+            case '2':   instOut = "cmovl";          //Prints the data for a move less than
                         break;
 
-            case '3':   instOut = "cmove";
+            case '3':   instOut = "cmove";          //Prints the data for a move equal
+                        break;
+        
+            case '4':   instOut = "cmovne";         //Prints the data for a move not equal
                         break;
 
-            case '4':   instOut = "cmovne";
+            case '5':   instOut = "cmovge";         //Prints the data for a move greater than or equal
                         break;
 
-            case '5':   instOut = "cmovge";
-                        break;
-
-            case '6':   instOut = "cmovg";
+            case '6':   instOut = "cmovg";          //Prints the data for a move greater than
                         break;
 
             default:    std::cout << "illegalop";
@@ -236,23 +230,22 @@ void twoBytes(/*const*/ char* tok)
         }
         std::cout << instOut << " ";
         checkRegisters(tok[3], tok[2]);
-        //std::cout << "\n";
     }
 
     else if (tok[0] == '6')
     {
         switch (tok[1])
         {
-            case '0':   instOut = "addq ";
+            case '0':   instOut = "addq ";          //Prints the data for an addition operation
                         break;
 
-            case '1':   instOut = "subq ";
+            case '1':   instOut = "subq ";          //Prints the data for a subtraction operation
                         break;
 
-            case '2':   instOut = "andq ";
+            case '2':   instOut = "andq ";          //Prints the data for an and operation
                         break;
 
-            case '3':   instOut = "xorq ";
+            case '3':   instOut = "xorq ";          //Prints the data for an exclusive or operation
                         break;
 
             default:    std::cout << "illegalop";
@@ -260,102 +253,80 @@ void twoBytes(/*const*/ char* tok)
         }
         std::cout << instOut << " ";
         checkRegisters(tok[3], tok[2]);
-        //std::cout << "\n";
     }
 
     else if ((tok[0] == 'A' || tok[0] == 'a') && tok[1] == '0')
     {
         std::cout << "pushq ";
         checkRegisters(tok[2], 0);
-        //std::cout << "\n";
     }
 
     else if ((tok[0] == 'B' || tok[0] == 'b') && tok[1] == '0')
     {
         std::cout << "popq ";
         checkRegisters(tok[2], 0);
-        //std::cout << "\n";
     }
 
     else
     {
         std::cout << "illegalop";
     }
-    //std::cout << instOut;
+
     std::cout << "\n";
 }
 
-void fourBytes(const char* tok)
-{
-    //Is this even used?    
-}
-
-void eightBytes(/*const*/ char* tok)
+//Prints the quad, flipping the endianness of the passed value
+void eightBytes(char* tok)
 {
     std::string instOut = ".quad ";
     char arr [16];
     arr[16] = '\0';
+
     for (int i = 0; i < 16; i++)
     {
-        arr[i] = tok[i];
+        arr[i] = tok[i];                            //Copies the value into a new array for flipping
     }
-    flipEndQuad(arr);
 
-    /*for (int i = 0, j = 15; i < 8; i = i + 2, j = j - 2)
-    {
-        char temp = arr[i];
-        arr[i] = arr[j - 1];
-        arr[j - 1] = temp;
-        temp = arr[i + 1];
-        arr[i + 1] = arr[j];
-        arr[j] = temp;
-    }*/
+    flipEndQuad(arr);
     std::cout << ".quad 0x";
     std::cout << arr << "\n";
-    /*std::stringstream inte;
-    inte << tok;
-    int swap;
-    inte >> swap;
-    std::cout << "Before swap: " << std::hex << swap;
-    swap = __builtin_bswap64(swap);
-    std::cout << "After: " << std::hex << swap;*/
-    //std::cout << instOut << "\n";
-
-    //std::cout << instOut << std::hex << swap << "\n";
 }
 
-void nineBytes(/*const*/ char* tok)
+//Determines if an instruction that is 9 bytes long is a jump, jump conditional, or a call
+void nineBytes(char* tok)
 {
+    std::string instOut;
     char arr [17];
     arr[16] = '\0';
+
     for (int i = 0, j = 2; i < 16; i++, j++)
     {
-        arr[i] = tok[j];
+        arr[i] = tok[j];                            //Copies the address/destination value for flipping
     }
-    std::string instOut;
+
     if (tok[0] == '7')
     {
         switch (tok[1])
         {
-            case '0':   instOut = "jmp";
+            case '0':   instOut = "jmp";            //Prints data for a jump always
                         break;
 
-            case '1':   instOut = "jle";
+            case '1':   instOut = "jle";            //Prints data for a jump less than or equal
                         break;
             
-            case '2':   instOut = "jl";
+            case '2':   instOut = "jl";             //Prints data for a jump less than
                         break;
 
-            case '3':   instOut = "je";
+            case '3':   instOut = "je";             //Prints data for a jump equal
                         break;
 
-            case '4':   instOut = "jne";
+            case '4':   instOut = "jne";            //Prints data for a jump not equal
                         break;
 
-            case '5':   instOut = "jge";
+            case '5':   instOut = "jge";            //Prints data for a jump greater than or equal
                         break;
 
-            case '6':   instOut = "jg";
+            case '6':   instOut = "jg";             //Prints data for a jump greater than
                         break;
 
             default:    std::cout << "illegalop\n";
@@ -363,35 +334,35 @@ void nineBytes(/*const*/ char* tok)
         }
         std::cout << instOut << " ";
         flipEndOther(arr);
-        std::cout << "\n";
     }
     
-    else if (tok[0] == '8' && tok[1] == '0')
+    else if (tok[0] == '8' && tok[1] == '0')        //Prints data for a call
     {
-        //instOut = "call\n";
         std::cout << "call ";
         flipEndOther(arr);
-        std::cout << "\n";
     }
 
     else 
     {
-        std::cout << "illegalop\n";
+        std::cout << "illegalop";
     }
 
-    //d::cout << instOut;
+    std::cout << "\n";
 }
 
-void tenBytes(/*const*/ char* tok)
+//Determines if an instruction that is 10 bytes long is an irmovq, rmmovq, or mrmovq
+void tenBytes(char* tok)
 {
     std::string instOut;
     char arr [17];
     arr[16] = '\0';
+    
     for (int i = 0, j = 4; i < 16; i++, j++)
     {
-        arr[i] = tok[j];
+        arr[i] = tok[j];                        //Copies the address/destination value for flipping
     }
-    if (tok[0] == '3' && tok[1] == '0')
+
+    if (tok[0] == '3' && tok[1] == '0')         //Prints data for irmovq
     {
         std::cout << "irmovq $";
         flipEndOther(arr);
@@ -399,7 +370,7 @@ void tenBytes(/*const*/ char* tok)
         checkRegisters(0, tok[3]);
     }
 
-    else if (tok[0] == '4' && tok[1] == '0')
+    else if (tok[0] == '4' && tok[1] == '0')    //Prints data for rmmovq
     {
         std::cout << "rmmovq ";
         checkRegisters(tok[2], 0);
@@ -410,7 +381,7 @@ void tenBytes(/*const*/ char* tok)
         std::cout << ")";
     }
 
-    else if (tok[0] == '5' && tok[1] == '0')
+    else if (tok[0] == '5' && tok[1] == '0')    //Prints data for mrmovq
     {
         std::cout << "mrmovq $";
         flipEndOther(arr);
@@ -424,13 +395,14 @@ void tenBytes(/*const*/ char* tok)
     {
         std::cout << "illegalop";
     }
-    //std::cout << instOut;
     std::cout << "\n";    
 }
 
-void readInst(/*const*/ char* tok, int length)
+//Reads in the length in bytes of each instruction, calls the appropriate function
+void readInst(char* tok, int length)
 {
     int bytes = 0;
+    
     for (int i = 0; i < length; i++)
     {
         if (tok[i] != '\n' && tok[i] != ' ' && tok[i] != '\t')
@@ -438,29 +410,27 @@ void readInst(/*const*/ char* tok, int length)
             bytes++;
         }
     }
+
     bytes = bytes / 2;
-    //char firstByte = tok[1];
+
     switch (bytes)
     {
-        case 0: std::cout << "\n";
+        case 0: std::cout << "\n";  //Prints a newline if the instruction is empty
                 break;
 
-        case 1: oneByte(tok);
+        case 1: oneByte(tok);       //Calls oneByte for an instruction 1 byte long
                 break;
 
-        case 2: twoBytes(tok);
+        case 2: twoBytes(tok);      //Calls twoBytes for an instruction 2 bytes long
                 break;
 
-        case 4: fourBytes(tok);
+        case 8: eightBytes(tok);    //Calls eightBytes for an instruction 8 bytes long
                 break;
 
-        case 8: eightBytes(tok);
+        case 9: nineBytes(tok);     //Calls nineBytes for an instruction 9 bytes long
                 break;
 
-        case 9: nineBytes(tok);
-                break;
-
-        case 10:tenBytes(tok);
+        case 10:tenBytes(tok);      //Calls tenBytes for an instruction 10 bytes long
                 break;
 
         default:std::cout << "An error occured while converting byte to instruction\n"; 
@@ -468,60 +438,50 @@ void readInst(/*const*/ char* tok, int length)
     }
 }
 
+//Begins disassembly by splitting the address from the instruction and calling readInst
 void diss(std::ifstream &r)
 {
     std::string line;
     while(r)
     {
-        //int linePos = 0;
         std::string delim = ": ";
         std::string tok;
-        //linePos = line.find(delim);
-        //std::string s = getline(r);
-        //std::stringstream stm(line);
-        std::getline(r, line);//, ": ");
+        std::getline(r, line);
         std::istringstream stm(line);
-        int lineNum = 1;
-        while(std::getline(stm, tok, ':'))
+        int lineNum = 1;                            //Tracks which line is address and which is instruction
+        while(std::getline(stm, tok, ':'))          //Splits at delim :
         {
             if (lineNum % 2 == 0)
             {
-                tok = tok.substr(1, tok.length());
+                tok = tok.substr(1, tok.length());  //Removes the leading space from instructions
             }
 
             if (tok.at(1) == 'x' || tok.at(1) == 'X')
             {
-                std::cout << tok << ":    ";
+                std::cout << tok << ":    ";        //Prints the memory address
             }
 
             else
             {
                 char char_array [tok.length()];
                 std::strcpy(char_array, tok.c_str());
-                readInst(char_array, tok.length());
-                //readInst(tok.c_str(), tok.length());
+                readInst(char_array, tok.length()); //Calls readInst on y86 instructions
             }
-            
-            //std::cout << tok << "\n";
             lineNum++;
         }
-        //std::string tok1 = line.substr(0, linePos);    
-        //std::string tok2 = line.substr(line.find(delim) + 2);
-        //line.trim();
-        //std::cout << tok1 + "\n";
-        //std::cout << tok2 + "\n";
     }
 }
 
+//Reads in a file from command line arguments. Program will stop if file name is not valid.
 int main(int argc, char * argv[])
 {
-    std::ifstream reader;
-    reader.open(argv[1]);
-    if (!reader)
+    std::ifstream reader;                           //Open an ifstream
+    reader.open(argv[1]);                           //Try to open the file passed as command line argument
+    if (!reader)                                    //Check if file was able to be opened
     {
         std::cerr << "Unable to open file, check the filename.\n";
-        exit(1); //System will stop if file cannot be found
+        exit(1);                                    //System will stop if file cannot be found
     }
-    diss(reader);
-    reader.close();
+    diss(reader);                                   //Call the disassembly function
+    reader.close();                                 //Close the ifstream before exiting
 }
